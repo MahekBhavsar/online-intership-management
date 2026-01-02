63dae90651c2c004f30929ce00c5f0c0a7ef7eca
MahekBhavsar <mahek.bhavsar29@gmail.com>
2026-01-02 11:39:43 +0530

    firebaseconfig done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Firestore } from '@angular/fire/firestore';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  // 2. Add it to the imports array
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('online-intership-management');
+export class App implements OnInit {
+  private firestore = inject(Firestore);
+
+  connectionStatus = signal('Connecting...');
+  isSuccess = signal(false);
+
+  ngOnInit() {
+    if (this.firestore) {
+      this.connectionStatus.set('Firebase Connection Successful! ✅');
+      this.isSuccess.set(true);
+    }
+  }
 }
