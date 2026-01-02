fb20385e92420072636e000443cf81e7cd68c20a
prachipatel2204-ops <prachipatel2204@gmail.com>
2026-01-02 15:56:45 +0530

    Create Reactive Form

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Login } from './users/login/login';
 import { Registration } from './users/registration/registration';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Registration],
+  imports: [RouterOutlet, Registration],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/users/registration/registration.ts
+++ src/app/users/registration/registration.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+interface registrationUser {
+  name: FormControl<string | null>;
+  email: FormControl<string | null>;
+  contact: FormControl<string | null>;
+  collegename: FormControl<String | null>;
+  degree: FormControl<string | null>;
+  year: FormControl<string | null>;
+  companyName: FormControl<string | null>;
+  professionalField: FormControl<string | null>;
+  gender: FormControl<string | null>;
+  cv: FormControl<string | null>;
+  address: FormControl<string | null>;
 
+}
 @Component({
   selector: 'app-registration',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './registration.html',
   styleUrl: './registration.css',
 })
 export class Registration {
-
+  registerForm = new FormGroup<registrationUser>({
+    name: new FormControl(null, [Validators.required]),
+    contact: new FormControl(null, [Validators.required]),
+    collegename: new FormControl(null),
+    degree: new FormControl('', [Validators.required]),
+    year: new FormControl('', [Validators.required]),
+    companyName: new FormControl(null),
+    professionalField: new FormControl(null, [Validators.required]),
+    gender: new FormControl(null, [Validators.required]),
+    cv: new FormControl(null, [Validators.required]),
+    address: new FormControl(null, [Validators.required]),
+    email: new FormControl(null, [Validators.required]),
+  })
+  onSubmit() {
+    console.log("Submit");
+  }
 }
