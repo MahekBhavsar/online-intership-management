c8cb469112b924411682f05a67f591559266ace0
Harshil Sureja <harshilsureja184@gmail.com>
2026-01-02 17:12:17 +0530

    DashBoard Code Ui Update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,35 +1,26 @@
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
 import { Dashboard } from './users/dashboard/dashboard';
 import { Navbar } from './users/navbar/navbar';
 import { Footer } from './users/footer/footer';
 import { Header } from './users/header/header';
-
-export const routes: Routes = [
-    {path: "dashboard", component: Dashboard},
-    {path: "navbar" , component: Navbar},
-    {path: "header", component: Header},
-    {path: "footer", component : Footer},
-    {path: "" , redirectTo : "dashboard" , pathMatch : "full"}
-=======
 import { AdminDashboard } from './admin/admin-dashboard/admin-dashboard';
-import { Dashboard } from './users/dashboard/dashboard';
 import { ManagedPIntershiprogram } from './admin/managed-intership-program/managed-intership-program';
 import { ManagedCourse } from './admin/managed-course/managed-course';
 import { ManagedStaff } from './admin/managed-staff/managed-staff';
 
 export const routes: Routes = [
+    { path: "dashboard", component: Dashboard },
+    { path: "navbar", component: Navbar },
+    { path: "header", component: Header },
+    { path: "footer", component: Footer },
     {
         path: 'admin/dashboard',
         component: AdminDashboard
     },
-    {
-        path: 'user/dashboard',
-        component: Dashboard
-    },
+    
     {
         path: 'admin/managed-intership',
-        component: ManagedPIntershiprogram 
+        component: ManagedPIntershiprogram
     },
     {
         path: 'admin/managed-course',
@@ -44,7 +35,8 @@ export const routes: Routes = [
         redirectTo: 'user/dashboard',
         pathMatch: "full"
 
-    }
+    },
 
->>>>>>> 2343fa5dc20d34d24213ff53aaf0059ff29737c4
+    { path: "", redirectTo: "dashboard", pathMatch: "full" }
+
 ];
