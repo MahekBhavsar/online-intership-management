4611a1e621fc3aab1f541c5e9a9c83d50f206b8a
MahekBhavsar <mahek.bhavsar29@gmail.com>
2026-01-02 17:45:03 +0530

    did final dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,18 @@ import { Dashboard } from './users/dashboard/dashboard';
 import { ManagedPIntershiprogram } from './admin/managed-intership-program/managed-intership-program';
 import { ManagedCourse } from './admin/managed-course/managed-course';
 import { ManagedStaff } from './admin/managed-staff/managed-staff';
+import { Login } from './users/login/login';
+import { AdminLogin } from './admin/Adminlogin/admin-login';
 
 export const routes: Routes = [
     {
         path: 'admin/dashboard',
         component: AdminDashboard
+    },
+    {
+        // 2. Add the Login route
+        path: 'admin/admin-login',
+        component: AdminLogin
     },
     {
         path: 'user/dashboard',
